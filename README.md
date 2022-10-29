be94403091d92b7bb0153d6a9c13790f34ff674b
Khanh Nguyen <khanhnm@live.com>
2022-10-29 08:39:37 +0700

    build macos app

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ![screenshot](screenshot.png)
 - Translate text in clipboard every 3 seconds.
 - GUI version of <https://github.com/mhwgoo/cambridge>
+- Build macOS app:
+```shell
+python3 setup.py py2app
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'optimize': 2,
+    'includes': ['wx', 'ansi2html', 'pyperclip'],
+    'excludes': ['numpy', 'gtk'],
+    'plist': {
+        'CFBundleName': 'Translators',
+    },
+    'arch': 'x86_64',
+    'iconfile': 'icon.icns'
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
